c88ace52c71c4d0abe732c1773c999b6154293cf
David White <dpwhite2@ncsu.edu>
2011-01-24 16:57:38 -0500

    Created rudimentary setup.py; put remote Celery commands in control.py.

---
+++ celerymanagementlib/celery_imports.py
@@ -0,0 +1,3 @@
+import celerymanagementlib.control
+import celerymanagementlib.tasks
+
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='celerymanagementlib',
+      version='0.0.0',
+      packages=['celerymanagementlib'],
+      )
