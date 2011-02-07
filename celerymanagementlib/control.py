e44629bd1f4b0905042ae3c6b36083a9296f7eed
David White <dpwhite2@ncsu.edu>
2011-02-07 13:19:31 -0500

    Improved behavior in the face of exceptions.

--- celerymanagementlib/control.py
+++ celerymanagementlib/control.py
@@ -1,3 +1,5 @@
+import traceback
+
 from celery.worker.control import Panel
 from celery.registry import tasks as tasks_registry
 from celery.task.base import BaseTask
@@ -17,30 +19,66 @@ def _get_task_class(name):
 def update_tasks_settings(panel, tasks_settings):
     # task_settings = dict: { taskname: { attrname: value, ... }, ... }
     panel.logger.debug('policy.control: update_tasks_settings()')
+    errors = []
     for taskname, settings in tasks_settings.iteritems():
-        # TODO: wrap this in try..except so one failure doesn't stop setting all settings.
-        taskcls = _get_task_class(taskname)
-        for attr,val in settings.iteritems():
-            setattr(taskcls, attr, val)
-    return {"ok": "task settings updated reset"}
+        try:
+            taskcls = _get_task_class(taskname)
+            for attr,val in settings.iteritems():
+                setattr(taskcls, attr, val)
+        except Exception as e:
+            errors.append((taskname, settings, traceback.format_ex()))
+    if errors:
+        msg = 'Errors occurred while attempting to update task settings.\n'
+        for taskname, settings, tb in errors:
+            m = 'Taskname: {0}\nSettings: {1}\n\n'.format(taskname, settings)
+            m += '{0}\n'.format(tb)
+            msg += m
+        panel.logger.error(msg)
+        
+        errmsg = "Exceptions occurred while updating task settings.  "
+        errmsg += "See the celeryd log for details."
+        return {"error": errmsg}
+    else:
+        return {"ok": "task settings updated"}
     
 
 @Panel.register
 def get_task_settings(panel, tasknames=None, setting_names=None):
+    """ Returns the settings actually set on a Task class without taking any 
+        inheritance into account.  It does *not* get settings from superclasses. 
+    """
     panel.logger.debug('policy.control: get_task_settings()')
     if tasknames is None:
         tasknames = tasks_registry.data
     allsettings = {}
+    errors = []
     for taskname in tasknames:
-        taskcls = _get_task_class(taskname)    
-        data = dict((attr, taskcls.__dict__[attr]) for attr in setting_names 
-                                                   if attr in taskcls.__dict__)
-        allsettings[taskname] = data
+        try:
+            taskcls = _get_task_class(taskname)
+            data = dict((attr, taskcls.__dict__[attr]) for attr in setting_names 
+                                                       if attr in taskcls.__dict__)
+            allsettings[taskname] = data
+        except Exception as e:
+            errors.append((taskname, traceback.format_ex()))
+    if errors:
+        msg = 'Errors occurred while attempting to retrieve task settings.\n'
+        for taskname, tb in errors:
+            m = 'Taskname: {0}\n\n{1}\n'.format(taskname,tb)
+            msg += m
+        panel.logger.error(msg)
+        
+        errmsg = "Exceptions occurred while retrieving task settings.  "
+        errmsg += "See the celeryd log for details."
+        return {"error": errmsg}
+        
     return allsettings
     
     
 @Panel.register
 def get_all_task_settings(panel, setting_names):
+    """ Returns the settings actually set on a Task class without taking any 
+        inheritance into account.  It does *not* get settings from superclasses. 
+    """
     panel.logger.debug('policy.control: get_all_task_settings()')
     allsettings = {}
     for taskname in tasks_registry.data:
@@ -56,9 +94,14 @@ def restore_task_settings(panel, restore_data):
     # restore_data = dict: { taskname: (restore_dict, erase_list), ... }
     panel.logger.debug('policy.control: restore_task_settings()\nrestore_data: {0}'.format(restore_data))
     
+    errors = []
     for taskname, (restore, erase) in restore_data.iteritems():
-        # TODO: catch taskname not found exceptions
-        taskcls = _get_task_class(taskname)
+        try:
+            taskcls = _get_task_class(taskname)
+        except tasks_registry.NotRegistered as e:
+            errors.append((taskname, traceback.format_ex()))
+            continue
+        
         for attr,v in restore.iteritems():
             panel.logger.debug('restoring: {0}.{1} = {2}'.format(taskname, attr, v))
             setattr(taskcls, attr, v)
@@ -69,6 +112,19 @@ def restore_task_settings(panel, restore_data):
             except AttributeError:
                 panel.logger.warning('restore_task_settings(): could not delete {0}.{1}'.format(taskname, attr))
                 pass
+    if errors:
+        msg = 'Errors occurred while attempting to restore task settings.\n'
+        for taskname, tb in errors:
+            m = 'Taskname: {0}\n\n{1}\n'.format(taskname,tb)
+            msg += m
+        panel.logger.error(msg)
+        
+        errmsg = "Exceptions occurred while restoring task settings.  "
+        errmsg += "See the celeryd log for details."
+        return {"error": errmsg}
+    else:
+        return {"ok": "task settings restored"}
+        
 
 #==============================================================================#
 @Panel.register
@@ -115,12 +171,30 @@ def set_task_attribute(panel, tasknames, attrname, value):
 #==============================================================================#
 @Panel.register
 def prefetch_increment(panel, n):
-    panel.consumer.qos.increment(n)
+    try:
+        panel.consumer.qos.increment(n)
+    except Exception as e:
+        m = 'Error occurred while attempting to increment the worker prefetch:'
+        m += '\n\n' + traceback.format_ex()
+        panel.logger.error(m)
+        
+        errmsg = "An exception occurred while incrementing the worker prefetch."
+        errmsg += "  See the celeryd log for details."
+        return {"error": errmsg}
     return {'ok': 'incremented prefetch by {0}'.format(n) }
 
 @Panel.register
 def prefetch_decrement(panel, n):
-    panel.consumer.qos.decrement(n)
+    try:
+        panel.consumer.qos.decrement(n)
+    except Exception as e:
+        m = 'Error occurred while attempting to decrement the worker prefetch:'
+        m += '\n\n' + traceback.format_ex()
+        panel.logger.error(m)
+        
+        errmsg = "An exception occurred while decrementing the worker prefetch."
+        errmsg += "  See the celeryd log for details."
+        return {"error": errmsg}
     return {'ok': 'decremented prefetch by {0}'.format(n) }
 
 #==============================================================================#
